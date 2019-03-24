481cb4ca2ceebfe4b547e9d506372fa7f17b844c
facccge <fcfc5702@qq.com>
2019-03-25 00:36:50 +0800

    completed Reduce getOrderedMedian

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -1,7 +1,10 @@
 package com.thoughtworks.collection;
 
+import org.apache.commons.collections.list.TreeList;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Reduce {
@@ -33,11 +36,43 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        if (arrayList.size() == 0) {
+            return 0;
+        }
+        double result = 0.0;
+        double sum = 0;
+        int count = 0;
+        for (int num : arrayList) {
+            sum += num;
+            count += 1;
+        }
+        result = sum / count;
+        return result;
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        double result = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int num : arrayList) {
+            list.add(num);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                int current = list.get(i);
+                int next = list.get(j);
+                if (current > next) {
+                    list.set(i, next);
+                    list.set(j, current);
+                }
+            }
+        }
+        int size = list.size();
+        if (size % 2 == 0) {
+            result = (list.get(size / 2) + list.get(size / 2 - 1)) / 2.0;
+        } else {
+            result = list.get(size / 2);
+        }
+        return result;
     }
 
     public int getFirstEven() {
